e1282a58850bc24d972dcb00784ea0ef6a147676
Green Camper <stefan.palacios@gmail.com>
2023-03-04 00:54:34 -0600

    Added pseudocode for scripting

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+/*
+-The weather dashboard page will utilize a weather API to 
+dynamically pull info and display on page
+-A search request will have to be sent to the api
+-Below this search bar there must be a recent history of cities selected
+we need
+
+--a function to call the api 
+---this function must also contain a button listener for the search button
+
+--a handler to sort through the data
+
+--a function to search for a specific city and pull that information
+---into a div
+---specifically getting these items from the JSON 
+----City name
+----Temp
+----Humidity
+
+--a function that saves the cities previously called into local storage
+--
+*/
